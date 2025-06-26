84580575b892caa4fc14f2a52a35661f7b442eea
AidenTag <87206143+AidenTag@users.noreply.github.com>
2025-06-26 11:53:57 -0600

    Update eval_sufficiency.sh

--- eval_sufficiency.sh
+++ eval_sufficiency.sh
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=0 python eval.py \
    --save_exp_code sufficiency \
    --task pathology_sufficiency \
    --k 1 \
-   --drop_out 0.5 \
+   --drop_out 0.25 \
    --model_type clam_sb \
    --results_dir results \
    --data_root_dir /scratch/alpine/$USER/pave_training \
