c3712ba018cecf2a0f4eba31163f22e65fab983d
AidenTag <87206143+AidenTag@users.noreply.github.com>
2025-07-02 11:21:18 -0600

    Revert "Add multi-label support for pathology sufficiency task"
    This reverts commit b722df70df13f5407f388a44a99060f457e821d8.

--- create_splits_seq.py
+++ create_splits_seq.py
@@ -12,7 +12,7 @@ parser.add_argument('--seed', type=int, default=1,
                     help='random seed (default: 1)')
 parser.add_argument('--k', type=int, default=10,
                     help='number of splits (default: 10)')
-parser.add_argument('--task', type=str, choices=['task_1_tumor_vs_normal', 'task_2_tumor_subtyping', 'pathology_full_subtyping', 'pathology_sufficiency', 'pathology_normalcy', 'pathology_sufficiency_multi_label'])
+parser.add_argument('--task', type=str, choices=['task_1_tumor_vs_normal', 'task_2_tumor_subtyping', 'pathology_full_subtyping', 'pathology_sufficiency', 'pathology_normalcy'])
 parser.add_argument('--val_frac', type=float, default= 0.1,
                     help='fraction of labels for validation (default: 0.1)')
 parser.add_argument('--test_frac', type=float, default= 0.1,
@@ -58,16 +58,6 @@ elif args.task == 'pathology_sufficiency':
                             seed = args.seed, 
                             print_info = True,
                             label_dict = {'insufficient':0, 'sufficient':1},
-                            patient_strat=False,
-                            ignore=[])
-
-elif args.task == 'pathology_sufficiency_multi_label':
-    args.n_classes=6
-    dataset = Generic_WSI_Classification_Dataset(csv_path = 'dataset_csv/pathology_sufficiency.csv',
-                            shuffle = False, 
-                            seed = args.seed, 
-                            print_info = True,
-                            label_cols = ['insufficient', 'scant_material', 'blurry', 'mucus', 'scant_cells', 'inflammation'],
                             patient_strat=False,
                             ignore=[])
 
